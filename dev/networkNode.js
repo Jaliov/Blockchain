efc5932238b7ca20b4e36ea0ed8790f766c19607
Joel Rudin <jarud55@gmail.com>
2022-01-14 21:15:33 -0500

    match blocks, transactions

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -222,6 +222,22 @@ app.get('/consensus', function(req, res ) {
   });
 });
 
+app.get('/block/:blockHash', function(req, res) { //localhost:3001/block/
+    const blockHash = req.params.blockHash; 
+    const correctBlock = bitcoin.getBlock(blockHash);
+    res.json({
+      block: correctBlock
+    })
+});
+
+app.get('/transcaction/:transactionId', function(req, res) {
+
+});
+
+app.get('/address/:address', function(req, res) {
+
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 })
